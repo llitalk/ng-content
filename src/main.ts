2cfd6e2b97b6cd010aa5db3f8fdc533d09228c29
llitalk <litalkaminsky@gmail.com>
2024-12-05 10:05:34 +0200

    create

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { ChildOneComponent } from './app/child-one/child-one.component';
 
 @Component({
   selector: 'app-root',
+  imports:[ChildOneComponent],
   template: `
     <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <p>Projection</p>
+    <section>
+   <app-child-one>
+   <h4 class="title">New Iphone</h4>
+   <img  class="img" src="https://media.istockphoto.com/id/1569559602/vector/3d-realistic-high-quality-smartphone-mockup-with-isolated-background-smart-phone-mockup.jpg?s=1024x1024&w=is&k=20&c=-W88FXvwL91fMdpkdjnGZwRpKddg49xuR9zoYhRznmQ="/>
+   </app-child-one>
+
+   <app-child-one>
+   <h4 class="title">New Smasung</h4>
+   <img  class="img" src="https://media.istockphoto.com/id/1569559602/vector/3d-realistic-high-quality-smartphone-mockup-with-isolated-background-smart-phone-mockup.jpg?s=1024x1024&w=is&k=20&c=-W88FXvwL91fMdpkdjnGZwRpKddg49xuR9zoYhRznmQ="/>
+   </app-child-one>
+
+
+   <app-child-one>
+   <h4 class="title">New Mac</h4>
+   <img  class="img" src="https://media.istockphoto.com/id/1569559602/vector/3d-realistic-high-quality-smartphone-mockup-with-isolated-background-smart-phone-mockup.jpg?s=1024x1024&w=is&k=20&c=-W88FXvwL91fMdpkdjnGZwRpKddg49xuR9zoYhRznmQ="/>
+   </app-child-one>
+    </section>
+  
   `,
 })
 export class App {
